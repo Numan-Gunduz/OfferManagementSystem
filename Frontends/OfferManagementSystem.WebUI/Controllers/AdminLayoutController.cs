ca7b26a57375fbad837cecaad277236dc1275dd3
213311012 <numangunduz00@gmail.com>
2024-03-14 01:53:29 +0300

    Temanın Dahil Edilmesi,Admin Layout Controllerlarının yazılması

---
+++ Frontends/OfferManagementSystem.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OfferManagementSystem.WebUI.Controllers
+{
+	public class AdminLayoutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	
+	}
+}
